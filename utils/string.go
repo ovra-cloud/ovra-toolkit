0ca12ef46cbb96f664d42b9d37dd2677a470fab8
ovra <ut1221@icloud.com>
2026-01-06 00:14:39 +0800

    feat(utils): 添加字符串分割转换为整数切片功能

--- utils/string.go
+++ utils/string.go
@@ -1,8 +1,31 @@
 package utils
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func StrAtoi(str string) int64 {
 	atoi, _ := strconv.ParseInt(str, 10, 64)
 	return atoi
+}
+
+func SplitToInt64(s, sep string) ([]int64, error) {
+	if s == "" {
+		return nil, nil
+	}
+	parts := strings.Split(s, sep)
+	res := make([]int64, 0, len(parts))
+	for _, p := range parts {
+		p = strings.TrimSpace(p)
+		if p == "" {
+			continue
+		}
+		v, err := strconv.ParseInt(p, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, v)
+	}
+	return res, nil
 }
